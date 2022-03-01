a1168c9fadd5cd61a65442205e7cffd6737ca934
PIMOT-Kuba <79198102+PIMOT-Kuba@users.noreply.github.com>
2022-03-01 13:11:47 +0100

    Bug: Fix merge errors

--- src/yolov4_trt_ros/utils/yolo_with_plugins.py
+++ src/yolov4_trt_ros/utils/yolo_with_plugins.py
@@ -20,26 +20,6 @@ except OSError as e:
                      'subdirectory?')
 
 
-<<<<<<< HEAD
-=======
-def _preprocess_yolo(img, input_shape):
-    """Preprocess an image before TRT YOLO inferencing.
-
-    # Args
-        img: int8 numpy array of shape (img_h, img_w, 3)
-        input_shape: a tuple of (H, W)
-
-    # Returns
-        preprocessed img: float32 numpy array of shape (3, H, W)
-    """
-    img = cv2.resize(img, (input_shape[1], input_shape[0]))
-    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-    img = img.transpose((2, 0, 1)).astype(np.float32)
-    img /= 255.0
-    return img
-
-
->>>>>>> dda16fa5ba7bc80db822cd6a331011c004000543
 def _nms_boxes(detections, nms_threshold):
     """Apply the Non-Maximum Suppression (NMS) algorithm on the bounding
     boxes with their confidence scores and return an array with the
@@ -83,60 +63,6 @@ def _nms_boxes(detections, nms_threshold):
     return keep
 
 
-<<<<<<< HEAD
-=======
-def _postprocess_yolo(trt_outputs, img_w, img_h, conf_th, nms_threshold=0.5):
-    """Postprocess TensorRT outputs.
-
-    # Args
-        trt_outputs: a list of 2 or 3 tensors, where each tensor
-                    contains a multiple of 7 float32 numbers in
-                    the order of [x, y, w, h, box_confidence, class_id, class_prob]
-        conf_th: confidence threshold
-
-    # Returns
-        boxes, scores, classes (after NMS)
-    """
-    # concatenate outputs of all yolo layers
-    detections = np.concatenate(
-        [o.reshape(-1, 7) for o in trt_outputs], axis=0)
-
-    # drop detections with score lower than conf_th
-    box_scores = detections[:, 4] * detections[:, 6]
-    pos = np.where(box_scores >= conf_th)
-    detections = detections[pos]
-
-    # scale x, y, w, h from [0, 1] to pixel values
-    detections[:, 0] *= img_w
-    detections[:, 1] *= img_h
-    detections[:, 2] *= img_w
-    detections[:, 3] *= img_h
-
-    # NMS
-    nms_detections = np.zeros((0, 7), dtype=detections.dtype)
-    for class_id in set(detections[:, 5]):
-        idxs = np.where(detections[:, 5] == class_id)
-        cls_detections = detections[idxs]
-        keep = _nms_boxes(cls_detections, nms_threshold)
-        nms_detections = np.concatenate(
-            [nms_detections, cls_detections[keep]], axis=0)
-    if len(nms_detections) == 0:
-        boxes = np.zeros((0, 4), dtype=np.int)
-        scores = np.zeros((0, 1), dtype=np.float32)
-        classes = np.zeros((0, 1), dtype=np.float32)
-    else:
-        xx = nms_detections[:, 0].reshape(-1, 1)
-        yy = nms_detections[:, 1].reshape(-1, 1)
-        ww = nms_detections[:, 2].reshape(-1, 1)
-        hh = nms_detections[:, 3].reshape(-1, 1)
-        boxes = np.concatenate([xx, yy, xx+ww, yy+hh], axis=1) + 0.5
-        boxes = boxes.astype(np.int)
-        scores = nms_detections[:, 4] * nms_detections[:, 6]
-        classes = nms_detections[:, 5]
-    return boxes, scores, classes
-
-
->>>>>>> dda16fa5ba7bc80db822cd6a331011c004000543
 class HostDeviceMem(object):
     """Simple helper data class that's a little nicer to use than a 2-tuple."""
 
@@ -267,7 +193,6 @@ class TrtYOLO(object):
 
         return inputs, outputs, bindings, stream
 
-<<<<<<< HEAD
     def preprocess_yolo(self, img, input_shape):
         """Preprocess an image before TRT YOLO inferencing.
 
@@ -339,12 +264,6 @@ class TrtYOLO(object):
         img_shape -- original image shape in format (H, W)
         """
 
-=======
-    def detect(self, img, conf_th=0.3):
-        """ Detect objects in the input image """
-
-        img_resized = _preprocess_yolo(img, self.input_shape)
->>>>>>> dda16fa5ba7bc80db822cd6a331011c004000543
         trt_outputs = []
 
         # Set host input to the image. The do_inference() function
@@ -394,19 +313,10 @@ class TrtYOLO(object):
         if self.cuda_ctx:
             self.cuda_ctx.pop()
 
-<<<<<<< HEAD
         boxes, scores, classes = self.postprocess_yolo(
             trt_outputs, img_shape[1], img_shape[0], conf_th)
 
         # clip x1, y1, x2, y2 within original image
         boxes[:, [0, 2]] = np.clip(boxes[:, [0, 2]], 0, img_shape[1]-1)
         boxes[:, [1, 3]] = np.clip(boxes[:, [1, 3]], 0, img_shape[0]-1)
-=======
-        boxes, scores, classes = _postprocess_yolo(
-            trt_outputs, img.shape[1], img.shape[0], conf_th)
-
-        # clip x1, y1, x2, y2 within original image
-        boxes[:, [0, 2]] = np.clip(boxes[:, [0, 2]], 0, img.shape[1]-1)
-        boxes[:, [1, 3]] = np.clip(boxes[:, [1, 3]], 0, img.shape[0]-1)
->>>>>>> dda16fa5ba7bc80db822cd6a331011c004000543
         return boxes, scores, classes
